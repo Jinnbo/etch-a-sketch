3a3fafa5dcd85d1b737f6aad5afec54c2f1df184
Jimmy <jimmyhui92012@gmail.com>
2022-07-17 22:40:23 -0700

    Implemented eraser and clear

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
+// Initial Variables
+let size = 16;
+let color = "black";
+
+// Variables
 const grid = document.getElementById('grid');
+const clear = document.getElementById('clear');
+const eraser = document.getElementById('eraser');
+
+// Event Listeners
+clear.addEventListener("click", resetGrid);
+
+let eraseOn = false;
+eraser.addEventListener("click", erase);
 
 
-grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
-grid.style.gridTemplateRows = `repeat(16, 1fr)`;
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
-for (let i = 0; i < 16*16; i++) {
+function setGrid(size){
 
-    const gridElement = document.createElement('div')
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    for (let i = 0; i < size*size; i++) {
 
-    gridElement.style.width = `${560/16}px`;
-    gridElement.style.height = `${560/16}px`;
-
-
-    gridElement.classList.add('grid-element')
-    grid.appendChild(gridElement)
+        let gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColor);
+        gridElement.addEventListener('mousedown', changeColor);
+        grid.appendChild(gridElement);
+    }
 }
+    
+function changeColor(e){
+
+    if (e.type === 'mouseover' && !mouseDown) return;
+
+    e.target.style.backgroundColor = color;
+    
+}
+
+function resetGrid(){
+    grid.innerHTML = '';
+    setGrid(size);
+}
+
+function erase(){
+
+    eraseOn = !eraseOn;
+
+    if (eraseOn){
+        color = "white";
+        eraser.classList.add('active');
+    } 
+    else{
+        color = "black";
+        eraser.classList.remove('active');
+    } 
+
+
+    console.log(eraseOn);
+
+}
+
+
+
+setGrid(size);
